df7897479fd86550063f09ad805a846062843987
Ankit Kumar Yadav <ankityadav18193@gmail.com>
2025-12-19 19:00:41 +0530

    Fixed missing slash in auth route

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(cors({
   origin: ["http://localhost:5173",
     "http://localhost:3000",
     "http://localhost:3001",
-     "https://gollo-frontend.vercel.app"], 
+     "https://gollo-frontend.vercel.app"],
   credentials: true
 }));
 
